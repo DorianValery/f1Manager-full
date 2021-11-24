7b489aff8f02c97a652c2b5da37f4293c5d21a93
Vincent <vincent.dehouve@supbioteh.fr>
2021-11-24 14:53:57 +0100

    pilote +inventaire +joueur

--- f1ManagerBack-v2/src/main/java/sopra/formation/web/JoueurRestController.java
+++ f1ManagerBack-v2/src/main/java/sopra/formation/web/JoueurRestController.java
@@ -1,5 +1,110 @@
 package sopra.formation.web;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.fasterxml.jackson.annotation.JsonView;
+
+import sopra.formation.model.Joueur;
+import sopra.formation.model.Views;
+import sopra.formation.repository.IJoueurRepository;
+
+@RestController
+@RequestMapping("/joueur")
+@CrossOrigin("*")
 public class JoueurRestController {
 
+	@Autowired
+	private IJoueurRepository joueurRepo;
+
+	@GetMapping("")
+	@JsonView(Views.ViewJoueur.class)
+	public List<Joueur> findAll() {
+		List<Joueur> joueurs = joueurRepo.findAll();
+
+		return joueurs;
+	}
+
+	@GetMapping("{id}")
+	@JsonView(Views.ViewJoueur.class)
+	public Joueur find(@PathVariable Long id) {
+		Optional<Joueur> optJoueur = joueurRepo.findById(id);
+
+		if (optJoueur.isPresent()) {
+			return optJoueur.get();
+		} else {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Joueur non trouvé");
+		}
+	}
+	
+	@GetMapping("{id}/detail")
+	//@JsonView(Views.ViewJoueurDetail.class)
+	public Joueur detail(@PathVariable Long id) {
+		Optional<Joueur> optJoueur = joueurRepo.findById(id);
+
+		if (optJoueur.isPresent()) {
+			return optJoueur.get();
+		} else {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Joueur non trouvé");
+		}
+	}
+
+	@PostMapping("")
+	@JsonView(Views.ViewJoueur.class)
+	public Joueur create(@RequestBody Joueur joueur) {
+		joueur = joueurRepo.save(joueur);
+
+		return joueur;
+	}
+
+	@PutMapping("/{id}")
+	@JsonView(Views.ViewJoueur.class)
+	public Joueur update(@PathVariable Long id, @RequestBody Joueur joueur) {
+		if (!joueurRepo.existsById(id)) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Joueur non trouvé");
+		}
+
+		joueur = joueurRepo.save(joueur);
+
+		return joueur;
+	}
+
+	@PatchMapping("/{id}")
+	@JsonView(Views.ViewJoueur.class)
+	public Joueur partialUpdate(@PathVariable Long id, @RequestBody Map<String, Object> updates) {
+		if (!joueurRepo.existsById(id)) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Joueur non trouvé");
+		}
+
+		Joueur joueurFind = joueurRepo.findById(id).get();
+
+
+		joueurFind = joueurRepo.save(joueurFind);
+
+		return joueurFind;
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		if (!joueurRepo.existsById(id)) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Joueur non trouvé");
+		}
+		
+		joueurRepo.deleteById(id);
+	}
 }
--- f1ManagerBack-v2/src/main/java/sopra/formation/web/PiloteRestController.java
+++ f1ManagerBack-v2/src/main/java/sopra/formation/web/PiloteRestController.java
@@ -1,5 +1,110 @@
 package sopra.formation.web;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.fasterxml.jackson.annotation.JsonView;
+
+import sopra.formation.model.Pilote;
+import sopra.formation.model.Views;
+import sopra.formation.repository.IPiloteRepository;
+
+@RestController
+@RequestMapping("/pilote")
+@CrossOrigin("*")
 public class PiloteRestController {
 
+	@Autowired
+	private IPiloteRepository piloteRepo;
+
+	@GetMapping("")
+	@JsonView(Views.ViewPilote.class)
+	public List<Pilote> findAll() {
+		List<Pilote> pilotes = piloteRepo.findAll();
+
+		return pilotes;
+	}
+
+	@GetMapping("{id}")
+	@JsonView(Views.ViewPilote.class)
+	public Pilote find(@PathVariable Long id) {
+		Optional<Pilote> optPilote = piloteRepo.findById(id);
+
+		if (optPilote.isPresent()) {
+			return optPilote.get();
+		} else {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Pilote non trouvé");
+		}
+	}
+	
+	@GetMapping("{id}/detail")
+	//@JsonView(Views.ViewPiloteDetail.class)
+	public Pilote detail(@PathVariable Long id) {
+		Optional<Pilote> optPilote = piloteRepo.findById(id);
+
+		if (optPilote.isPresent()) {
+			return optPilote.get();
+		} else {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Pilote non trouvé");
+		}
+	}
+
+	@PostMapping("")
+	@JsonView(Views.ViewPilote.class)
+	public Pilote create(@RequestBody Pilote pilote) {
+		pilote = piloteRepo.save(pilote);
+
+		return pilote;
+	}
+
+	@PutMapping("/{id}")
+	@JsonView(Views.ViewPilote.class)
+	public Pilote update(@PathVariable Long id, @RequestBody Pilote pilote) {
+		if (!piloteRepo.existsById(id)) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Pilote non trouvé");
+		}
+
+		pilote = piloteRepo.save(pilote);
+
+		return pilote;
+	}
+
+	@PatchMapping("/{id}")
+	@JsonView(Views.ViewPilote.class)
+	public Pilote partialUpdate(@PathVariable Long id, @RequestBody Map<String, Object> updates) {
+		if (!piloteRepo.existsById(id)) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Pilote non trouvé");
+		}
+
+		Pilote piloteFind = piloteRepo.findById(id).get();
+
+
+		piloteFind = piloteRepo.save(piloteFind);
+
+		return piloteFind;
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		if (!piloteRepo.existsById(id)) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Pilote non trouvé");
+		}
+		
+		piloteRepo.deleteById(id);
+	}
 }
