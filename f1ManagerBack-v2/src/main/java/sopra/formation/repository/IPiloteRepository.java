58fa65787c9985f88f5187c2e9c60f21fbace586
Fabiola <fabiola.ouedraogo@gmail.com>
2021-12-02 12:03:49 +0100

    debut querry pilote (pas terminé, en comm)

--- f1ManagerBack-v2/src/main/java/sopra/formation/repository/IPiloteRepository.java
+++ f1ManagerBack-v2/src/main/java/sopra/formation/repository/IPiloteRepository.java
@@ -1,8 +1,13 @@
 package sopra.formation.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import sopra.formation.model.Pilote;
+
 
 public interface IPiloteRepository extends JpaRepository<Pilote, Long>{
 //findAllPilote
@@ -11,4 +16,17 @@ public interface IPiloteRepository extends JpaRepository<Pilote, Long>{
 //findPiloteByNationalité
 //findPiloteByJoueur
 //findPiloteByPrix
+	
+	
+	//Query findall Pilote
+	
+	//findbyID ou l’état est pris en compte (enabled / disabled) :
+	//@Query("select distinct p from Pilote p left join fetch p.etat e where p.id = :id")
+	//Optional<Pilote> findPiloteByIdWithEtat(@Param("id")Long id);
+	
+	//findbyID ou l’état est pris en compte (enabled / disabled) + le prix si disabled :
+	//@Query("select distinct p from Pilote p join fetch p.prix=p.etat where p.id = :id ") 
+	//Optional<Pilote> findPiloteByIdWithEtatAndPrix(@Param("id")Long id);
+	
+	
 }
