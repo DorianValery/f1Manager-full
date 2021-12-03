f94d9699cdf774a7f2cefe66640d74d9febb2de3
Dorian Valery <d.valery12@gmail.com>
2021-12-03 10:11:32 +0100

    Voiture inventaire

--- f1ManagerFront/src/app/app-routing.module.ts
+++ f1ManagerFront/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MenuComponent } from './menu/menu.component';
+import { PiloteInventaireComponent } from './pilote-inventaire/pilote-inventaire.component';
 import { VoitureInventaireComponent } from './voiture-inventaire/voiture-inventaire.component';
 
 const routes: Routes = [
-  // { path: "pilote", component: PiloteInventaireComponent },
+ { path: "pilote", component: PiloteInventaireComponent },
   // { path: "infrastructure", component: InfrastructureInventaireComponent }
   {path: "ecurie", component : MenuComponent},
   {path: "voiture", component: VoitureInventaireComponent}
