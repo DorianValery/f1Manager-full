1880d1c616d0beae6b653d16d62a604c758c8da8
Fabiola <fabiola.ouedraogo@gmail.com>
2021-12-08 14:44:05 +0100

    Correction inscription

--- f1ManagerFront/src/app/inscription-joueur/inscription-joueur.service.ts
+++ f1ManagerFront/src/app/inscription-joueur/inscription-joueur.service.ts
@@ -10,33 +10,39 @@ import { SeConnecterHttpService } from '../se-connecter/se-connecter-http.servic
 })
 export class InscriptionJoueurService {
 
+  inscriptionUrl: string;
   joueurUrl: string;
-  compteUrl:string;
+  compteUrl: string;
+  
 
   constructor(private http: HttpClient, private appConfig: AppConfigService, private seConnecter: SeConnecterHttpService) {
-    this.joueurUrl = this.appConfig.backEndUrl + "joueur/"
-    this.compteUrl = this.appConfig.backEndUrl + "compte/"
+    //this.joueurUrl = this.appConfig.backEndUrl + "joueur/"
+    //this.compteUrl = this.appConfig.backEndUrl + "compte/"
+    this.inscriptionUrl = this.appConfig.backEndUrl + "compte/inscription/"
   }
   findById(id: number): Observable<Joueur> {
-    return this.http.get<Joueur>(this.joueurUrl + id);
+    return this.http.get<Joueur>(this.inscriptionUrl + id);
   }
-  create(joueur: InscriptionDTO) {
-    this.http.post<InscriptionDTO>(this.joueurUrl, joueur).subscribe(resp => {
+  create(inscription: InscriptionDTO) {
+    /*this.http.post<InscriptionDTO>(this.compteUrl, inscription).subscribe(resp => {
 
     }, error => console.log(error));
-    this.http.post<InscriptionDTO>(this.compteUrl, joueur).subscribe(resp => {
+    this.http.post<InscriptionDTO>(this.joueurUrl, inscription).subscribe(resp => {
+
+    }, error => console.log(error));*/
+    this.http.post<InscriptionDTO>(this.inscriptionUrl, inscription).subscribe(resp => {
 
     }, error => console.log(error));
   }
-  modify(joueur: InscriptionDTO) {
-    this.http.put<InscriptionDTO>(this.joueurUrl + joueur.id, joueur).subscribe(resp => {
+  modify(inscription: InscriptionDTO) {
+    this.http.put<InscriptionDTO>(this.inscriptionUrl + inscription.id, inscription).subscribe(resp => {
 
     }, error => console.log(error));
     
   }
 
   deleteById(id: number) {
-    this.http.delete<void>(this.joueurUrl + id).subscribe(resp => {
+    this.http.delete<void>(this.inscriptionUrl + id).subscribe(resp => {
 
     }, error => console.log(error));
   }
